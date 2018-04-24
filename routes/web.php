cc6b6e25ee154184ddaa72886adcd1f4bf5726e1
vagrant <vagrant@homestead>
2018-04-24 01:40:17 +0000

    web

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes();
 
 // 个人页面
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

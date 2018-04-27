38fa00decb0449cb1a5f3361ef8cf81852790771
zb <zb@exands.com>
2018-04-27 11:01:26 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Auth::routes();
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 话题分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
